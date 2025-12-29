7db1811c9caa470c061e85571aa037160b5356c8
divyanshukrkumar-pixel <divyanshukr.kumar@gmail.com>
2025-12-30 00:50:15 +0530

    Add files via upload

---
+++ DEVA/__init__.py
@@ -0,0 +1,26 @@
+﻿# Authored By Certified Coders © 2025
+from Tune.core.bot import MusicBotClient
+from Tune.core.dir import StorageManager
+from Tune.core.git import git
+from Tune.core.userbot import Userbot
+from Tune.misc import dbb, heroku
+from Tune.logging import LOGGER
+
+StorageManager()
+git()
+dbb()
+heroku()
+
+app = MusicBotClient()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
---
+++ DEVA/core/dir.py
@@ -0,0 +1,15 @@
+﻿# Authored By Certified Coders © 2025
+import os
+from Tune.logging import LOGGER
+
+BASE_DIR = os.getcwd()
+DOWNLOAD_DIR = os.path.join(BASE_DIR, "downloads")
+CACHE_DIR = os.path.join(BASE_DIR, "cache")
+BACKUP_DIR = os.path.join(BASE_DIR, "TuneBackup")
+
+def StorageManager():
+
+    os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+    os.makedirs(CACHE_DIR, exist_ok=True)
+    os.makedirs(BACKUP_DIR, exist_ok=True)
+    LOGGER(__name__).info("Directories Updated.")
