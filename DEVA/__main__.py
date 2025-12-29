ff420e41b7dc320f0b8076c252b520237e0dafec
divyanshukrkumar-pixel <divyanshukr.kumar@gmail.com>
2025-12-30 01:39:29 +0530

    Update __main__.py

--- DEVA/__main__.py
+++ DEVA/__main__.py
@@ -6,12 +6,12 @@ from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from Tune import LOGGER, app, userbot
-from Tune.core.call import StreamController
-from Tune.misc import sudo
-from Tune.plugins import ALL_MODULES
-from Tune.utils.database import get_banned_users, get_gbanned
-from Tune.utils.cookie_handler import fetch_and_store_cookies 
+from DEVA import LOGGER, app, userbot
+from DEVA.core.call import StreamController
+from DEVA.misc import sudo
+from DEVA.plugins import ALL_MODULES
+from DEVA.utils.database import get_banned_users, get_gbanned
+from DEVA.utils.cookie_handler import fetch_and_store_cookies 
 from config import BANNED_USERS
 
 
@@ -28,9 +28,9 @@ async def init():
 
     try:
         await fetch_and_store_cookies()
-        LOGGER("Tune").info("ʏᴏᴜᴛᴜʙᴇ ᴄᴏᴏᴋɪᴇs ʟᴏᴀᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ✅")
+        LOGGER("DEVA").info("ʏᴏᴜᴛᴜʙᴇ ᴄᴏᴏᴋɪᴇs ʟᴏᴀᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ✅")
     except Exception as e:
-        LOGGER("Tune").warning(f"⚠️ᴄᴏᴏᴋɪᴇ ᴇʀʀᴏʀ: {e}")
+        LOGGER("DEVA").warning(f"⚠️ᴄᴏᴏᴋɪᴇ ᴇʀʀᴏʀ: {e}")
 
     await sudo()
 
@@ -42,21 +42,21 @@ async def init():
         for user_id in users:
             BANNED_USERS.add(user_id)
     except Exception as e:
-        LOGGER("Tune").warning(f"ғᴀɪʟᴇᴅ ᴛᴏ ʟᴏᴀᴅ ʙᴀɴɴᴇᴅ ᴜsᴇʀs: {e}")
+        LOGGER("DEVA").warning(f"ғᴀɪʟᴇᴅ ᴛᴏ ʟᴏᴀᴅ ʙᴀɴɴᴇᴅ ᴜsᴇʀs: {e}")
 
     await app.start()
 
     for all_module in ALL_MODULES:
-        importlib.import_module("Tune.plugins" + all_module)
+        importlib.import_module("DEVA.plugins" + all_module)
 
-    LOGGER("Tune.plugins").info("ᴛᴜɴᴇ's ᴍᴏᴅᴜʟᴇs ʟᴏᴀᴅᴇᴅ...")
+    LOGGER("DEVA.plugins").info("ᴛᴜɴᴇ's ᴍᴏᴅᴜʟᴇs ʟᴏᴀᴅᴇᴅ...")
     await userbot.start()
     await StreamController.start()
 
     try:
         await StreamController.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("Tune").error(
+        LOGGER("DEVA").error(
             "ᴘʟᴇᴀsᴇ ᴛᴜʀɴ ᴏɴ ᴛʜᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴏғ ʏᴏᴜʀ ʟᴏɢ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ.\n\nᴀɴɴɪᴇ ʙᴏᴛ sᴛᴏᴘᴘᴇᴅ..."
         )
         exit()
@@ -64,13 +64,13 @@ async def init():
         pass
 
     await StreamController.decorators()
-    LOGGER("Tune").info(
+    LOGGER("DEVA").info(
         "\x54\x75\x6e\x65\x20\x56\x69\x61\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("Tune").info("sᴛᴏᴘᴘɪɴɢ ᴛᴜɴᴇ ᴠɪᴀ ᴍᴜsɪᴄ ʙᴏᴛ ...")
+    LOGGER("DEVA").info("sᴛᴏᴘᴘɪɴɢ ᴛᴜɴᴇ ᴠɪᴀ ᴍᴜsɪᴄ ʙᴏᴛ ...")
 
 
 if __name__ == "__main__":
