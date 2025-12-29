5faf7b7304b96045c158bd19e38fe2bf5d7a0c88
divyanshukrkumar-pixel <divyanshukr.kumar@gmail.com>
2025-12-30 01:13:28 +0530

    Update settings.py

--- DEVA/plugins/bot/settings.py
+++ DEVA/plugins/bot/settings.py
@@ -4,8 +4,8 @@ from pyrogram.enums import ChatType
 from pyrogram.errors import MessageNotModified
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from Tune import app
-from Tune.utils.database import (
+from DEVA import app
+from DEVA.utils.database import (
     add_nonadmin_chat,
     get_authuser,
     get_authuser_names,
@@ -21,15 +21,15 @@ from Tune.utils.database import (
     skip_off,
     skip_on,
 )
-from Tune.utils.decorators.admins import ActualAdminCB
-from Tune.utils.decorators.language import language, languageCB
-from Tune.utils.inline.settings import (
+from DEVA.utils.decorators.admins import ActualAdminCB
+from DEVA.utils.decorators.language import language, languageCB
+from DEVA.utils.inline.settings import (
     auth_users_markup,
     playmode_users_markup,
     setting_markup,
     vote_mode_markup,
 )
-from Tune.utils.inline.start import private_panel
+from DEVA.utils.inline.start import private_panel
 from config import BANNED_USERS, OWNER_ID
 
 # ─── SETTINGS MESSAGE ──────────────────────────────────────────────
